7f107d8cbfb50e5e761ce957df3d0059be2bfb82
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2024-11-17 19:07:45 +0530

    Install nextjs ,Tailwind CSS and Flowbite React , also created First template for Headee

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'flowbite-react'
+
+export default function Header() {
+  return (
+    <div>
+        <Button>
+        Header
+        </Button>
+        Header</div>
+  )
+}
+
