4c3ce7908827b05be512ad15bed43896f2215432
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2024-11-17 19:50:14 +0530

    Add next-themes to apply dark mode

---
+++ src/app/components/ThemeCom.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+export default function ThemeCom({ children }) {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
